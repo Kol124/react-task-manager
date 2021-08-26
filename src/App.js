323ddbfa5a05d7ad4466f95258cd3f91c3d5a4bf
Kol124 <65050225+Kol124@users.noreply.github.com>
2021-08-26 17:44:41 +0100

    File Changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import "./App.css";
 import React from "react";
 import TaskContextProvider from "./context/TaskContext";
 import ThemeContextProvider from "./context/ThemeContext";
+import { ExpenseContextProvider } from "./context/ExpenseContext";
 import Main from "./components/Main";
 
 function App() {
   return (
     <ThemeContextProvider>
       <TaskContextProvider>
-        <div className="container">
-          <Main />
-        </div>
+        <ExpenseContextProvider>
+          <div className="container">
+            <Main />
+          </div>
+        </ExpenseContextProvider>
       </TaskContextProvider>
     </ThemeContextProvider>
   );
